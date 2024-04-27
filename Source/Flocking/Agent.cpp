0796cceb22cb1d0e29c3b0e512b7bfb6f97a95dc
jcannon64 <148830736+jcannon64@users.noreply.github.com>
2024-04-26 23:05:14 -0400

    Add files via upload

---
+++ Source/Flocking/Agent.cpp
@@ -0,0 +1,28 @@
+#include "Agent.h"
+#include "FlockingGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+AAgent::AAgent() {
+	PrimaryActorTick.bCanEverTick = true;
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AgentMesh"));
+	RootComponent = Mesh;
+	Velocity = FVector(0.f);
+}
+
+void AAgent::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void AAgent::Init(UStaticMeshComponent* mesh, int id) {
+	UE_LOG(LogTemp, Warning, TEXT("Agent initialized."));
+	Mesh->SetStaticMesh(mesh->GetStaticMesh());
+}
+
+void AAgent::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	AFlockingGameModeBase* gmb = Cast<AFlockingGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	Velocity = Velocity * gmb->Speed;
+
+	FVector loc = GetActorLocation();
+	SetActorLocation(loc + Velocity);
+}
